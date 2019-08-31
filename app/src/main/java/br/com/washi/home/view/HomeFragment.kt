461a68c37788fe7cd1958da651b13b248b62ea79
L <lfernandodesenv@gmail.com>
2019-08-31 19:56:57 -0300

    Coded generic adapter and holder for service itens in home fragment

--- app/src/main/java/br/com/washi/home/view/HomeFragment.kt
+++ app/src/main/java/br/com/washi/home/view/HomeFragment.kt
@@ -4,18 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import br.com.washi.R
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import br.com.washi.services.Service
+import br.com.washi.util.setUp
+import kotlinx.android.synthetic.main.card_wash_service.view.*
+import kotlinx.android.synthetic.main.content_home.*
 
-class HomeFragment : BottomSheetDialogFragment() {
+class HomeFragment : Fragment() {
+
+    private val service = listOf(
+        Service("Lavar"),
+        Service("Passar"),
+        Service("Entregar"),
+        Service("Outros")
+    )
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.content_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        configureRecyclerView()
+    }
+
+    private fun configureRecyclerView() {
+        rvList.setUp(service, R.layout.card_wash_service, {
+            nameProduct.text = it.serviceTitle
+        }, manager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false))
+    }
 }
